5563c5ba4030799a9553938c3795d4b5b1a1a03f
kissrt <kissrita96@yahoo.com>
2018-06-20 14:19:08 +0300

    Delete settings.py

--- Code/settings.py
+++
@@ -1,5 +0,0 @@
-# signatures file path
-
-DIRECTORY_PATH_MCYT ='C:\\Users\\pc\\Documents\\Allamvizsga\\Datasets\\MCYT_Bandi_Andras'
-DIRECTORY_PATH_MOBISIG ='C:\\Users\\pc\\Documents\\Allamvizsga\\Datasets\\MOBISIG'
-
